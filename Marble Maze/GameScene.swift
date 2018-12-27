05872db482c7632cd4862508ab9f49f854ffced5
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-27 20:48:36 +0300

    accelerometerData used

--- Marble Maze/GameScene.swift
+++ Marble Maze/GameScene.swift
@@ -8,9 +8,31 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
+    var player: SKSpriteNode!
+    
+    var lastTouchPosition: CGPoint?
+    
+    var motionManager: CMMotionManager!
+    
+    //MARK: -
+    
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 96, y: 672)
+        
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+        player.physicsBody?.allowsRotation = false
+        player.physicsBody?.linearDamping = 0.5
+        
+        player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+        player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
+        addChild(player)
+    }
     
     /// Loads wall at specified position
     ///
@@ -107,6 +129,11 @@ class GameScene: SKScene {
     //MARK: -
     override func didMove(to view: SKView) {
         
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates()
+        
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
@@ -114,26 +141,42 @@ class GameScene: SKScene {
         addChild(background)
         
         loadLevel()
+        createPlayer()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            lastTouchPosition = location
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            lastTouchPosition = location
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        lastTouchPosition = nil
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        lastTouchPosition = nil
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        #if targetEnvironment(simulator)
+        if let currentTouch = lastTouchPosition {
+            let diff = CGPoint(x: currentTouch.x - player.position.x, y: currentTouch.y - player.position.y)
+            physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+        }
+        #else
+        if let accelerometerData = motionManager.accelerometerData {
+            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.y * 50, dy: accelerometerData.acceleration.x * -50)
+        }
+        #endif
     }
 }
